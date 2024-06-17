14d50caf3ab50dac5ee72d284cdfaafdca497b72
Aef_SB <46419278+aeff60@users.noreply.github.com>
2024-06-17 14:22:21 +0700

    flask ugin jinja2 for template

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def home():
+#     return "Hello, Flask!"
+
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html', title="Home Page", heading="Welcome to My Website", content="This is the home page content.")
+
 
 @app.route('/about')
 def about():
